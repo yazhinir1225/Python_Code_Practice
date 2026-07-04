45ea981ce4de053fe3ce46c4fcfada522595ce5c
yazhinir1225 <yazhiniR1225@gmail.com>
2026-07-04 12:22:42 +0530

    patten

---
+++ .history/reactanglepatten_20260704103753.py
@@ -0,0 +1,11 @@
+
+#for i in range(1,6):
+    #for j in range(1,i+1):
+     #   print(j,end="")
+   # print()    
+///
+letters="ABCDE"  
+for i in range(1,6):
+    for j in range(i):
+        print(letters[j],end="")
+    print()    ///
---
+++ .history/reactanglepatten_20260704110606.py
@@ -0,0 +1,15 @@
+""""
+#for i in range(1,6):
+    #for j in range(1,i+1):
+     #   print(j,end="")
+   # print()    
+
+letters="ABCDE"  
+for i in range(1,6):
+    for j in range(i):
+        print(letters[j],end="")
+    print()    
+"""
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end='')
